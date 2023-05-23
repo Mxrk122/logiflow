28134121190e29f962e765960dc9e78497199cd2
Mxrk122 <60331995+Mxrk122@users.noreply.github.com>
2023-05-23 17:10:59 -0600

    componente router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Importa los módulos necesarios
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import PrivateRoute from '../components/PrivateRoute.jsx';
+import Main from '../routes/Main'
 
-function App() {
-  const [count, setCount] = useState(0)
+// Componente principal
+const App = () => {
+  const isAuthenticated = true; // Cambia esto con tu lógica de autenticación
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <PrivateRoute
+          path="/dashboard"
+          component={Dashboard}
+          isAuthenticated={isAuthenticated}
+        />
+        <Route render={() => <Redirect to="/login" />} />
+      </Switch>
+    </Router>
+  );
+};
 
-export default App
+export default App;
