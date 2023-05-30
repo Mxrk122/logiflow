a950aff1d0b60fd8060b9f23789a530903e2bdeb
Santiago Taracena <tar20017@uvg.edu.gt>
2023-05-30 16:24:04 -0600

    Finished address configuration

--- backend/server.js
+++ backend/server.js
@@ -21,4 +21,7 @@ app.use('/users', usersRouter)
 const branchesRouter = require('./routes/branches')
 app.use('/branches', branchesRouter)
 
+const locationsRouter = require('./routes/locations')
+app.use('/locations', locationsRouter)
+
 app.listen(process.env.SERVER_PORT || 4000, () => console.log('Listening...'))
