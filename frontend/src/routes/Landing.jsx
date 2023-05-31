29c94c95d4ea607108949dcca8d138f80b115b38
Mxrk122 <60331995+Mxrk122@users.noreply.github.com>
2023-05-31 14:56:06 -0600

    nada; ;

--- frontend/src/routes/Landing.jsx
+++ frontend/src/routes/Landing.jsx
@@ -6,13 +6,15 @@ const Landing = () => {
   const { user } = useContext(UserContext)
 
   return (
-    <>
-      <h1>Landing</h1>
-      <Link to="/profile">Profile Here</Link>
-      {user.admin ? <Link to="/landing/createbuilding">create building</Link> : null}
-      {user.admin ? <Link to="/landing/addVehicle">add vehicle</Link> : null}
-      {user.admin ? <Link to="/landing/vehicleOwns">Delete Vehicle</Link> : null}
-    </>
+    <div className='bg'>
+      <div className='main-container'>
+        <h1>Landing</h1>
+        <Link to="/profile">Profile Here</Link>
+        {user.admin ? <Link to="/landing/createbuilding">create building</Link> : null}
+        {user.admin ? <Link to="/landing/addVehicle">add vehicle</Link> : null}
+        {user.admin ? <Link to="/landing/vehicleOwns">Delete Vehicle</Link> : null}
+      </div>
+    </div>
   )
 }
 
