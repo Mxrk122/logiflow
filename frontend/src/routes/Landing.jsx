f0f7cadb2899e45351d21d9d7e42bee8b1aba7cf
Santiago Taracena <tar20017@uvg.edu.gt>
2023-05-31 15:06:59 -0600

    Solved merge conflict

--- frontend/src/routes/Landing.jsx
+++ frontend/src/routes/Landing.jsx
@@ -6,7 +6,6 @@ const Landing = () => {
   const { user } = useContext(UserContext)
 
   return (
-<<<<<<< HEAD
     <div className='bg'>
       <div className='main-container'>
         <h1>Landing</h1>
@@ -16,16 +15,6 @@ const Landing = () => {
         {user.admin ? <Link to="/landing/vehicleOwns">Delete Vehicle</Link> : null}
       </div>
     </div>
-=======
-    <>
-      <h1>Landing</h1>
-      <Link to="/profile">Profile Here</Link>
-      <Link to="/order">Make an Order</Link>
-      {user.admin ? <Link to="/landing/createbuilding">create building</Link> : null}
-      {user.admin ? <Link to="/landing/addVehicle">add vehicle</Link> : null}
-      {user.admin ? <Link to="/landing/vehicleOwns">Delete Vehicle</Link> : null}
-    </>
->>>>>>> bdb1ae20757126e53d37928d5f7417474b62c31f
   )
 }
 
